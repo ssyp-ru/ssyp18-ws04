31634ecf7720f7e16f3a688b7349f1f8f26b6e9b
Sergey <dfrreg@yandex.ru>
2018-07-05 20:33:22 +0700

    fghjkb

--- logging.lua
+++ logging.lua
@@ -1,4 +1,7 @@
 local function init(u)
+	local days = os.date (day)
+	local months = os.date (month)
+	f = io.open('logs.txt', 'a')
 	log={}
 	for i=1, #u do
 		if u[i].kind == 'sensor' then
@@ -11,13 +14,20 @@ local function init(u)
 end
 local function checkState()
 end
+--[[f = io.open('rtujm.txt', 'w')
+f:write('zapis')
+f:flush()
+f:close()--]]
 local function updateLog (dt)
+	--[[for i=1, #log do
+		f:write(tostring(log[i].oldstate)..'\n'..log[i].sensor.subkind..'\n')
+	end--]]
 	for i=1, #log do
 		if log[i].oldstate ~= log[i].sensor.state then
-			--print ('DETECT')
-			break
+			f:write(log[i].sensor.subkind..'	'..tostring(log[i].sensor.state)..'\n')
 		end
 	end
+	f:write()
 end
 
 return {init=init,checkState=checkState,updateLog=updateLog}
