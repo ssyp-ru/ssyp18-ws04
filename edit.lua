2cf02cecac72d1fa68321f2d5c786996defdd25b
Batman <zevs00100@yandex.ru>
2018-07-06 09:30:42 +0700

    asdaslkdgjs

--- edit.lua
+++ edit.lua
@@ -4,10 +4,18 @@ obj = require "constructor"
 		x2 = 0,
 		y1 = 0,
 		y2 = 0,
+		w1 = 0,
+		w2 = 0,
+		h1 = 0,
+		h2 = 0,
 	}
 
 function editWall()
-	edit.w = -(edit.x1 - edit.x2) 
-	edit.h = -(edit.y1 - edit.y2) 
-	u[#u+1] = obj.createWall(edit.x1, edit.y1, edit.w, edit.h)
+	edit.w1 = -(edit.x1 - edit.x2) 
+	edit.h1 = -(edit.y1 - edit.y2) 
+	u[#u+1] = obj.createWall(edit.x1, edit.y1, edit.w1, edit.h1)
+end
+
+function editHalfWall()
+	
 end
